03e792ebf1a084d1ec4e748e492ef5fac13fd8a1
Muhammad Konain <konainkhan0101@gmail.com>
2023-03-29 17:35:37 -0700

    Work Done

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env:{
+    CONTENTFUL_SPACE_ID:"wwrxbjld101r",
+    CONTENTFUL_ACCESS_KEY:"Go8Tz3_b3_8nd1d-kvrVzBbIXGRXvrTEy79bT0DhGjE"
+
+  }
 }
 
 module.exports = nextConfig
